78cabbf82862e48e380a696b5960cbd59567c6c8
a-kennedy10 <akennedy@kennedy-consultants.com>
2020-11-16 13:49:52 -0500

    created user model

---
+++ PetWatch/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  PetWatch
+//
+//  Created by Alex Kennedy on 11/16/20.
+//
+
+import Foundation
+
+class User {
+    var username: String
+    var email: String
+    
+    init(username: String, email: String) {
+        self.email = email
+        self.username = username
+    }
+}
+
