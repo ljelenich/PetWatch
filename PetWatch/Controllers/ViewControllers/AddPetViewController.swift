de86ab1cb8ba3c642e4bb9fdeddbf6ae47b8125f
Shean Bjoralt <shean@datamanifold.com>
2020-11-18 15:13:47 -0700

    changes to AddPetVC and PetListTableVC

--- PetWatch/Controllers/ViewControllers/AddPetViewController.swift
+++ PetWatch/Controllers/ViewControllers/AddPetViewController.swift
@@ -30,24 +30,11 @@ class AddPetViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     //MARK: - Actions
     
     @IBAction func savePetButtonTapped(_ sender: Any) {
+        
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
