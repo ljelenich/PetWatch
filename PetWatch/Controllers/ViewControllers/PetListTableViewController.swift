012cd55f81a3f35b4e0bdbe062311685c5c2f002
Shean Bjoralt <shean@datamanifold.com>
2020-11-19 16:12:17 -0700

    edits to EmailVC and PetsListVC

--- PetWatch/Controllers/ViewControllers/PetListTableViewController.swift
+++ PetWatch/Controllers/ViewControllers/PetListTableViewController.swift
@@ -31,26 +31,41 @@ class PetListTableViewController: UIViewController {
         } catch let signOutErr {
             print("Failed to sign out:", signOutErr)
         }
-
-    }
-    
-    @IBAction func addPetButtonTapped(_ sender: Any) {
-        
     }
     
     @IBAction func aspcaInfoButtonTapped(_ sender: Any) {
-        
+        UIApplication.shared.open(URL(string: "https://www.aspca.org/pet-care/general-pet-care/emergency-care-your-pet")! as URL, options: [:], completionHandler: nil)
     }
 }
 
 extension PetListTableViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return PetController.shared.pets.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let petToDisplay = PetController.shared.pets[indexPath.row]
+        UITableViewCell().textLabel?.text = petToDisplay.name
+        UITableViewCell().detailTextLabel?.text = petToDisplay.breed
+        UITableViewCell().imageView?.image = petToDisplay.profileImage
+        
         return UITableViewCell()
 
     }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        
+//    }
+//    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        
+//    }
 }
