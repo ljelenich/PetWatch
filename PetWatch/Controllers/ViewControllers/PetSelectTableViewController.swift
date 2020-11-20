8022be7b2255652dd6639acb0fb11d47caf5180d
a-kennedy10 <akennedy@kennedy-consultants.com>
2020-11-20 14:12:10 -0500

    commit to access storyboard

--- PetWatch/Controllers/ViewControllers/PetSelectTableViewController.swift
+++ PetWatch/Controllers/ViewControllers/PetSelectTableViewController.swift
@@ -13,7 +13,7 @@ class PetSelectTableViewController: UITableViewController {
     // MARK: - Outlets
     
     // MARK: - Properties
-    let pets: [Pet?] = []
+    var pets: [Pet?] = []
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -78,4 +78,42 @@ class PetSelectTableViewController: UITableViewController {
         }
     }
 
+}
+
+// MARK: - Extensions
+
+extension PetSelectTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        if let photo = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//            selectPhotoButton.setTitle("", for: .normal)
+//            photoImageView.image = photo
+//            delegate?.photoSelectorViewControllerSelected(image: photo)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func presentImagePickerActionSheet() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        let actionSheet = UIAlertController(title: "Pick a photo", message: nil, preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            actionSheet.addAction(UIAlertAction(title: "Photos", style: .default, handler: { (_) in
+                imagePickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+                self.present(imagePickerController, animated: true, completion: nil)
+            }))
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (_) in
+                imagePickerController.sourceType = UIImagePickerController.SourceType.camera
+                self.present(imagePickerController, animated: true)
+            }))
+        }
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(actionSheet, animated: true)
+    }
 }
