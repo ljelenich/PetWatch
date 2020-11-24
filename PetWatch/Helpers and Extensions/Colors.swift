3d367630f832752426f328b5915e66e619522d69
LAURA JELENICH <ljelenich85@gmail.com>
2020-11-24 11:10:22 -0500

    Added custom colors

---
+++ PetWatch/Helpers and Extensions/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  PetWatch
+//
+//  Created by LAURA JELENICH on 11/24/20.
+//
+
+import UIKit
+
+extension UIColor {
+    static func tealColor() -> UIColor? {
+       return UIColor(named: "tealColor")
+    }
+    
+    static func grayColor() -> UIColor? {
+       return UIColor(named: "grayColor")
+    }
+}
